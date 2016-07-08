4344b45f3b1ad4a30c960c4b2601043c8178c875
Jason Wang <jason.wang@icrar.org>
2016-07-08 18:59:12 +0800

    cleaned writeBench

--- writeBench/write_benchmark.cc
+++ writeBench/write_benchmark.cc
@@ -19,70 +19,11 @@
 // Any bugs, questions, concerns and/or suggestions please email to
 // lbq@shao.ac.cn
 
-
 #include "../ConcatParallelTable.h"
 #include "../NolockParallelTable.h"
 #include "../AsmParallelTable.h"
 #include <string>
 #include "../timing/tictak.h"
-
-
-
-
-/*
-
-void concatTables(Block<String> &names)
-{
-    Table concTab (names, Block<String>(), Table::Old, TSMOption(), "SUBDIR");
-    concTab.rename (tablename, Table::New);
-}
-
-void checkTable (uInt nrow)
-    ///void checkTable (const Table& tab, uInt nkey, uInt nsubrow, Int stval,
-    ///		 Bool reorder=True, uInt nrow=10)
-{
-    Table tab(tablename);
-    AlwaysAssertExit (tab.nrow() == nrow);
-
-    ArrayColumn<Float> data(tab, "data");
-    //cout<<data.get(0)<<endl;
-    for (uInt i=0; i<tab.nrow(); i++) {
-        Array<float> data_s=data.get(i);
-        //2 d converted to a d array
-        Vector<float> data_s_con=data_s.reform(IPosition(1, data_s.nelements()));
-        Vector<float> data_s_rf=data_arr.reform(IPosition(1, data_arr.nelements()));
-
-        for(int j=0; j<data_arr.nelements(); j++){
-            if(data_s_rf[j] != data_s_con[j]){
-                cout << "row = " << i << ", column = data, element = " << j << endl;
-                cout << "write data value = " << data_s_con[j] << ", data_arr value = " << data_s_rf[j] << endl;
-                exit(-1);
-            }
-        }
-
-    }
-}
-
-// First build a description.
-void addColumns(ParallelTable *tab)
-{
-
-    // Build the table description.
-    TableDesc td("", "1", TableDesc::Scratch);
-    // Now create a new table from the description.
-    SetupNewTable newtab(name, td, Table::New);
-    Table tab(newtab, nrrow);
-
-
-    // define column objects and link them to the table
-
-    for (Int i=0; i<nrrow; ++i) {
-        data_col.put (i, data_arr);
-    }
-}
-
-
-    */
 
 
 int main(int argc, char **argv)
@@ -109,16 +50,20 @@ int main(int argc, char **argv)
         tablename = argv[4];
     }
 
+    // generate some data
     IPosition array_shape = IPosition(2, xsize, ysize);
     Array<Float> data_arr = Array<Float>(array_shape);
     indgen (data_arr);
 
+    // define parallel table, add columns, and create table
     ParallelTable *tab = new ConcatParallelTable(tablename, rows, mpiSize, mpiRank);
+//    ParallelTable *tab = new AsmParallelTable(tablename, rows, mpiSize, mpiRank);
+//    ParallelTable *tab = new NolockParallelTable(tablename, rows, mpiSize, mpiRank);
     tab->addColumn (ArrayColumnDesc<Float>("data", array_shape, ColumnDesc::Direct));
     tab->createTable();
 
+    // put data in, which is the same as serial CTDS tables
     ArrayColumn<Float> data_col(*(tab->get_table()), "data");
-
     for (int i=0; i<tab->rows(); i++){
         int row = tab->row(i);
         data_arr = row+1;
@@ -127,68 +72,6 @@ int main(int argc, char **argv)
     }
 
     delete tab;
-
-    //according mpi process rank to define filename
-    /*
-    stringstream filename_s;
-    filename_s << "tConcatTable3_tmp.tab" << mpiRank;
-    filename = filename_s.str();
-
-
-
-    Block<String> names(mpiSize);
-    int source;
-    char recvfilename[100];
-
-    //  send filename to process 0
-    if(mpiRank>0){
-        MPI_Send(filename.c_str(),strlen(filename.c_str())+1,MPI_CHAR,0,99,MPI_COMM_WORLD);
-        //cout<<"Rank="<<mpiRank<<","<<"filename="<<filename<<endl;
-    }
-    else{ //process 0 receive other process filename
-        names[0]=filename;
-        for(source=1;source<mpiSize;source++){
-            MPI_Recv(recvfilename,sizeof(recvfilename),MPI_CHAR,source,99,MPI_COMM_WORLD,&status);
-            // cout<<"Rank="<<source<<","<<"filename="<<recvfilename<<sizeof(recvfilename)<<endl;
-            string s(recvfilename);
-            names[source]=s;
-            //  cout<<"Rank="<<source<<","<<"filename="<<names[source]<<endl;
-        }
-    }
-
-    try {
-        MPI_Barrier(MPI_COMM_WORLD);
-        tictak_add((char*)filename.c_str(),0);
-        createTable (filename, NrRows); //
-        MPI_Barrier(MPI_COMM_WORLD);
-        tictak_add((char*)"end",0);
-        //process 0 concatTable and checkTable
-        if(mpiRank==0){
-            concatTables(names);
-            //  if will check Table delete follow comment
-            //    checkTable (NrRows*mpiSize);
-
-            float Seconds = tictak_total(0,0);
-            unsigned long long CellSize = atoi(argv[2])*atoi(argv[3])*sizeof(float);
-            unsigned long long TableSize = CellSize * NrRows * mpiSize;
-            int Mps = TableSize / Seconds / 1000000;
-
-            cout << "MB/s," << Mps;
-            cout << ",Seconds," << Seconds;
-            cout << ",TableSize(Byte)," << TableSize;
-            cout << ",NrRows," << NrRows;
-            cout << ",CellSize(Byte)," << CellSize;
-            cout << ",MpiSize," << mpiSize;
-            cout << ",Xlength," << atoi(argv[2]);
-            cout << ",Ylength," << atoi(argv[3]);
-            cout << endl;
-        }
-    } catch (AipsError x) {
-        cout << "Exception caught: " << x.getMesg() << endl;
-        return 1;
-    }
-    */
-
 
     MPI_Finalize();
     return 0;
